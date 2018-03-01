897023ade577027055a81ca51d822798a6b8907a
ballardcz <ballardcz@gmail.com>
2018-03-01 17:50:33 -0500

    fixing star wars https file

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -27,35 +27,54 @@ $(document).ready(function () {
     numbers = getRndInteger(1, 12);
     toBeat = getRndInteger(19, 120);
     console.log(toBeat);
+    console.log(numbers);
 
-    var green = numbers;
-    var blue = numbers;
-    var yellow = numbers;
+    var green = numbers* Math.round(Math.random() * 10);
+    var blue = numbers* Math.round(Math.random() * 10);
+    var yellow = numbers* Math.floor(Math.random() + 3);
     var red = numbers;
 
     $(".red").on("click", function () {
       currentScore += red;
-      $("#currentScore").html(currentScore)
+      $("#currentScore").html("<h2>" + "Your Score:" + currentScore + "</h2>")
+    });
+    $(".blue").on("click", function () {
+      currentScore += blue;
+      $("#currentScore").html("<h2>" + "Your Score:" + currentScore + "</h2>")
+    });
+    $(".green").on("click", function () {
+      currentScore += green;
+      $("#currentScore").html("<h2>" + "Your Score:" + currentScore + "</h2>")
+    });
+     $(".yellow").on("click", function () {
+      currentScore += yellow;
+      $("#currentScore").html("<h2>" + "Your Score:" + currentScore + "</h2>")
     });
     
-    console.log(red);
+    console.log(red+ "red");
+    console.log(green+ "green");
+    console.log(blue+ "blue");
+    console.log(yellow+ "yellow");
+
     $('#first').removeClass('hidden');
 
     $('#start').addClass("hidden");
 
     $("#timer").html("<h3>" + "Target Number:" + toBeat + "</h3>");
     $("#currentScore").html("<h2>" + "Your Score:" + currentScore + "</h2>");
-  };
+
+  if (currentScore >= toBeat) {
+    alert("you lose!");
+    // intervalId = setInterval(decrement, 5000);
+
+  }
+};
 
   // if ($("#red").on("click", red)) {
   //   console.log(red)
   // } else if ($("#green").on("click", green)) {
   //   console.log(green)
   // };
-  if (currentScore >= 121) {
-    alert("you lose!");
-    intervalId = setInterval(decrement, 5000);
-
-  }
+  
   
 });
